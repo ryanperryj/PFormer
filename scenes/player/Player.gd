867a6036ece47fa5bd816c63da9d59ce3a1e80eb
Ryan <ryanperryj@gmail.com>
2021-02-24 10:00:43 -0500

    Fixed jumping

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -9,8 +9,6 @@ const GRAVITY = 1000
 
 const JUMP_FORCE = 280
 
-var on_floor = false
-
 var jump_charge = 0
 var velocity = Vector2.ZERO
 
@@ -26,22 +24,11 @@ func _physics_process(delta):
 		$Sprite.flip_h = x_input < 0
 		velocity.x = move_toward(velocity.x, MAX_SPEED * x_input, ACCELERATION * delta)
 	
-	#on_floor = $Floorcheck.get_overlapping_bodies().size() != 0
-	
-	if on_floor:
+	#if on_floor:
+	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = -JUMP_FORCE
-
+	
 	velocity.y = move_toward(velocity.y, FALL_SPEED, GRAVITY * delta)
 	
-	velocity = move_and_slide(velocity)
-
-
-func _on_Floorcheck_body_entered(body):
-	on_floor = true
-	print('true')
-
-
-func _on_Floorcheck_body_exited(body):
-	on_floor = false
-	print('false')
+	velocity = move_and_slide(velocity, Vector2.UP)
